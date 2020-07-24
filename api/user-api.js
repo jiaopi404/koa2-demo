5035fe0820979aa8cf1530aebfe95eabf91b99c3
jiaopi404 <1240659804@qq.com>
2020-07-25 02:34:48 +0800

    update 添加 删除用户接口

--- api/user-api.js
+++ api/user-api.js
@@ -10,10 +10,11 @@ const GET_USER_BY_ID = '/get_user/:id' // get { params: id }
 const REGISTER = '/register' // post { data: { username, password } }
 const LOGIN = '/login' // post { data: { username, password } }
 const GET_USER_INFO = '/get_user_info' // get
+const DEL_USER = '/del_user/:id' // post { params: id }
 
 router.get(GET_ALL_USER, async ctx => {
   try {
-    const result = await User.findAll()
+    const result = await User['GET_ALL_USERS']()
     if (result) {
       ctx.body = {
         code: 200,
@@ -43,10 +44,7 @@ router.post(REGISTER, async ctx => {
     if (!password) {
       ctx.body = { code: -1, msg: '缺失密码', data: ctx.request.body }
     }
-    const result = await User.create({
-      username,
-      password
-    })
+    const result = await User['ADD_USER']({ username, password })
     ctx.body = { code: 200, msg: '注册成功', data: result }
   } catch (err) {
     ctx.body = { code: 500, msg: '服务器内部错误', message: err.message }
@@ -74,7 +72,7 @@ router.post(LOGIN, async ctx => {
       const _token = await JWT.sign({
         payload: { id: thatUser.id, username: thatUser.username }
       })
-      ctx.body = { code: 200, msg: '注册成功', data: result, _token }
+      ctx.body = { code: 200, msg: '登录成功', data: result, _token }
     } else {
       ctx.body = { code: -1, msg: '登录失败', data: ctx.request.body }
     }
@@ -90,8 +88,22 @@ router.get(GET_USER_BY_ID, async ctx => {
       ctx.body = { code: -1, msg: '缺失 id 参数', data: {} }
       return
     }
-    const queryResult = await User.getUserInfoById({ id })
+    const queryResult = await User['GET_USER_INFO_BY_ID']({ id })
     ctx.body = { code: 200, data: queryResult }
+  } catch (err) {
+    ctx.body = { code: 500, msg: '服务器内部错误', message: err.message }
+  }
+})
+
+router.post(DEL_USER, async ctx => {
+  try {
+    const { id } = ctx.params
+    if (!id) {
+      ctx.body = { code: -1, msg: '缺失 id 参数', data: {} }
+      return
+    }
+    const queryResult = await User['DEL_USER_BY_ID']({ id })
+    ctx.body = { code: 200, data: queryResult, msg: '删除成功' }
   } catch (err) {
     ctx.body = { code: 500, msg: '服务器内部错误', message: err.message }
   }
@@ -100,8 +112,7 @@ router.get(GET_USER_BY_ID, async ctx => {
 router.get(GET_USER_INFO, async ctx => {
   try {
     const { id } = ctx.JWT
-    console.log('jwt id: ', id)
-    const queryResult = await User.getUserInfoById({ id })
+    const queryResult = await User['GET_USER_INFO_BY_ID']({ id })
     ctx.body = { code: 200, data: queryResult }
   } catch (err) {
     ctx.body = { code: 500, msg: '服务器内部错误', message: err.message }
